5311397f1a7f15b8f8b243c448c5b86a27930b01
David <david.lml.95@gmail.com>
2023-11-30 09:37:14 -0400

    improved baseLayers

--- components/map.tsx
+++ components/map.tsx
@@ -15,6 +15,9 @@ type Props = {
 const MainMap = ({ changeBtn, trigger }: Props) => {
     const { width } = useWindowSize()
 
+    const BING_API_KEY = 'AuhiCJHlGzhg93IqUH_oCpl_-ZUrIE6SPftlyGYUvr9Amx5nzA-WqGcPquyFZl4L';
+    const yttrium_attribution = '<a href="https://yttrium-technology.com/" target="_blank">Yttrium</a>';
+
     return (
         <div id="map">
             <MapContainer scrollWheelZoom
@@ -22,7 +25,12 @@ const MainMap = ({ changeBtn, trigger }: Props) => {
                 zoom={8}
                 className='w-full h-full z-0'>
                 <LayersControl position={width! <= 768 ? "topright" : "topleft"} >
-                    <LayersControl.BaseLayer checked name="arcgisonline">
+                    <LayersControl.BaseLayer checked name="google">
+                        <TileLayer  attribution='&copy; Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+                            url="http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}" />
+                    </LayersControl.BaseLayer>
+
+                    <LayersControl.BaseLayer name="arcgisonline">
                         <TileLayer attribution='&copy; Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
                             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
                     </LayersControl.BaseLayer>
